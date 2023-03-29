aed4b79a4dbe7ee2a8a881f07d05f4e53a0e7c9e
hasyed <hasansyed12s@outlook.com>
2023-03-29 17:24:10 -0400

    fxml

--- src/main/java/org/example/presentation/view/App.java
+++ src/main/java/org/example/presentation/view/App.java
@@ -1,90 +1,25 @@
 package org.example.presentation.view;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import org.example.HibernateModule;
+import org.example.business.CemsService;
+import javafx.fxml.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.util.Objects;
 
 public class App extends Application {
+    public static Injector injector = Guice.createInjector(new HibernateModule());
+    CemsService service = new CemsService(injector);
     @Override
-    public void start(Stage primaryStage) throws FileNotFoundException {
-        // Create a border pane for the window
-        Pane root = new Pane();
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(425);
-        rectangle.setY(100);
-        rectangle.setWidth(350.0);
-        rectangle.setHeight(450.0);
-        rectangle.setFill(Color.GREY);
-        root.getChildren().add(rectangle);
-
-        // Create a box for the login form
-        VBox loginBox = new VBox(10);
-        loginBox.setPadding(new Insets(20));
-
-        // Add a title to the login form
-        Text title = new Text("Algoma University Login");
-        title.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        title.setFill(Color.WHITE);
-        title.setLayoutX(480);
-        title.setLayoutY(150);
-        root.getChildren().add(title);
-
-        // Add an email field to the login form
-        TextField emailField = new TextField();
-        emailField.setPromptText("Email");
-        emailField.setPrefSize(220,20);
-        HBox box = new HBox(5);
-        box.setPadding(new Insets(275, 20 , 20, 470));
-        box.getChildren().add(emailField);
-        loginBox.getChildren().add(box);
-
-        // Add a password field to the login form
-        TextField passwordField = new TextField();
-        passwordField.setPromptText("Password");
-        passwordField.setPrefSize(220,20);
-        HBox box2 = new HBox(5);
-        box2.setPadding(new Insets(5, 10 , 10, 470));
-        box2.getChildren().add(passwordField);
-        loginBox.getChildren().add(box2);
-
-        // Add a login button to the login form
-        Button loginButton = new Button("Login");
-        loginButton.setPrefSize(80,20);
-        loginButton.setLayoutX(560);
-        loginButton.setLayoutY(480);
-
-        loginButton.setOnAction(event -> {
-
-        });
-        root.getChildren().add(loginButton);
-        root.getChildren().add(loginBox);
-
-        // Create a scene and show the window
-        Scene scene = new Scene(root, 1200, 700);
-        primaryStage.setTitle("AlgomaU Login");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("LoginWindow.fxml")));
+        primaryStage.setTitle("App");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
     }
 
---
+++ src/main/java/org/example/presentation/view/LoginController.java
@@ -0,0 +1,18 @@
+package org.example.presentation.view;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    
+    public Button loginButton;
+
+    public Label titleLabel;
+
+    public PasswordField passwordField;
+
+    public TextField emailField;
+
+}
